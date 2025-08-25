1b2b9a8d37e69a78d4f6012540edb0ad015ce853
K. Arthur Endsley <endsley@umich.edu>
2025-08-25 13:38:41 -0600

    Initial fit of new rehydration model with VPD, PPFD

--- src/06_rehydration_time_stomatal_model_fitting.R
+++ src/06_rehydration_time_stomatal_model_fitting.R
@@ -86,7 +86,8 @@ df.out <- df %>%
   mutate(datetime.rel = datetime - dhours(HOUR.START),
     DOY.rel = as.integer(format(datetime.rel, '%j')),
     hour.rel = hour(datetime.rel) + minute(datetime.rel) / 60) %>%
-  left_join(select(df.met, datetime.rel, Dmean), by = 'datetime.rel')
+  left_join(select(df.met, datetime.rel, Dmean), by = 'datetime.rel') %>%
+  ungroup()
 
 # Diagnostics
 df.out %>%
@@ -94,6 +95,36 @@ df.out %>%
 ggplot(mapping = aes(x = datetime, y = WP_m)) +
   geom_line() +
   facet_wrap(~ DOY.rel, scales = 'free', ncol = 2)
+
+# Interpolating met data
+df.clean <- df.out %>%
+  select(datetime, datetime.rel, DOY.rel, hour.rel, t, PAR.MJm2, Dmean, WP_m) %>%
+  group_by(DOY.rel) %>%
+  mutate(Dmean = median(Dmean, na.rm = T)) %>%
+  fill(PAR.MJm2, .direction = 'downup')
+
+
+# Tom's model (example) ########################################################
+
+# Simulate the differential equation, forward in time
+model.stomatal <- function (
+    ts, vpd = 1000, irr = 0.5, psi0 = -3, tau = 4, psi.source = -2, k = 1, c1 = 1, c2 = 1) {
+  wp <- numeric(length = length(ts)) # Output vector of WP
+  # Initialize the WP time series with WP at first step
+  wp[1] <- psi0
+  # Now, each WP of each successive time step depends on the last
+  for (t in 2:length(ts)) {
+    dt <- ts[t] - ts[t-1]
+    wp[t] <- wp[t-1] + (dt/tau) * (psi.source - wp[t-1] - (c1 * irr[t-1] * vpd[t-1]) / ((irr[t-1] + k) * (1 + c2 * vpd[t-1])))
+  }
+  return(wp)
+}
+
+# Confirmed we get the same result as Tom's script
+result <- model.stomatal(1:10,
+  vpd = c(30,20,20,10,5,5,5,3,3,3),
+  irr = c(1000,900,700,400,50,0,0,0,0,0),
+  tau = 5, psi0 = -2, psi.source = 0, c1 = 0.3, c2 = 0.1, k = 500)
 
 
 # Model fitting framework ######################################################
@@ -105,17 +136,109 @@ rmse <- function (observed, predicted) {
   return((1/n) * sum(sqrt((predicted - observed)^2), na.rm = T))
 }
 
-model.stomatal <- function (
-    ts, vpd = 1000, irr = 0.5, psi0 = -3, tau = 4, psi.source = -2, k = 1, c1 = 1, c2 = 1) {
-  wp <- numeric(length = length(ts))
-  wp[1] <- psi0 + (1/tau) * (psi.source - psi0 - (c1 * irr * vpd) / ((irr + k) * (1 + c2 * vpd)))
-  for (t in ts[2:length(ts)]) {
-    wp[t] <- wp[t-1] + (1/tau) * (psi.source - wp[t-1] - (c1 * irr * vpd) / ((irr + k) * (1 + c2 * vpd)))
-  }
-  return(wp)
+rmse.model.stomatal <- function (params, df = df.clean) {
+  ts <- df$t
+  vpd <- df$Dmean
+  irr <- df$PAR.MJm2
+  psi0 <- df$WP_m[1]
+  tau <- params[1]
+  psi.source <- params[2]
+  k <- params[3]
+  c1 <- params[4]
+  c2 <- params[5]
+  pred <- model.stomatal(ts, vpd, irr, psi0, tau, psi.source, k, c1, c2)
+  return(with(df, rmse(WP_m, pred)))
 }
 
-plot(model.stomatal(1:24), type = 'l', bty = 'n', ylab = 'Water Potential (MPa)',
-  lwd = 2, xaxt = 'n', ylim = c(-3, -2))
-abline(h = -2)
-axis(1, at = seq(0, 20, 5), labels = c(16, 20, 24, 4, 8))
+# Example
+# with(filter(df.clean, DOY.rel == 110),
+#   plot(model.stomatal(t, Dmean, PAR.MJm2, psi0 = WP_m[1]),
+#     type = 'l', bty = 'n', ylab = 'Water Potential (MPa)', lwd = 2, xaxt = 'n'))
+# abline(h = -2)
+# axis(1, at = seq(0, 42, 6), labels = seq(0, 42, 6) / 2)
+
+
+# Fitting the model ############################################################
+
+# A data frame with the unique DOYs
+df.doy <- df.clean %>%
+  group_by(DOY.rel) %>%
+  summarize()
+fit.params <- matrix(nrow = length(df.doy$DOY.rel), ncol = 1 + 5)
+fit.scores <- matrix(nrow = nrow(fit.params), ncol = 1)
+for (i in 1:nrow(fit.params)) {
+  doy <- df.doy$DOY.rel[i]
+  df.sub <- filter(df.clean, DOY.rel == doy)
+  if (nrow(df.sub) == 0) next() # Skip empty subsets
+  wrap.rmse <- function (params) {
+    rmse.model.stomatal(params, df = df.sub)
+  }
+  # Parameters are: tau, psi.source, k, c1, c2
+  result <- optim(c(6, -1, 100, 0.1, 0.2), wrap.rmse, method = 'L-BFGS-B',
+    lower = c(0, -6, 0, 0, 0.1), upper = c(24, 0, 1000, 1, 1))
+    # control = list(pgtol = 0.01))
+  fit.params[i,1] <- doy
+  fit.params[i,2:6] <- result$par
+  fit.scores[i,] <- wrap.rmse(result$par)
+}
+
+df.params <- bind_cols(
+  as.data.frame(fit.params) %>%
+    rename(DOY.rel = V1, tau = V2, psi.source = V3, k = V4, c1 = V5, c2 = V6),
+  as.data.frame(fit.scores) %>%
+    rename(RMSE = V1)) %>%
+  # NOTE: We need dynamic inputs for this function
+  right_join(df.clean, by = 'DOY.rel')
+
+
+# Plotting results #############################################################
+
+df.params %>%
+  left_join(df.met, by = 'DOY.rel') %>%
+  mutate(rain = if_else(ppt_mm > 1, DOY.rel, NA)) %>%
+ggplot(mapping = aes(x = DOY.rel, y = RMSE)) +
+  geom_vline(aes(xintercept = rain), color = 'darkblue', linetype = 'dotted',
+    linewidth = 1) +
+  geom_bar(stat = 'identity') +
+  scale_x_continuous(expand = c(0, 0)) +
+  labs(x = 'Day of Year', y = 'Model RMSE (MPa)') +
+  theme_minimal()
+# ggsave(width = 6.5, height = 3, dpi = 172, bg = 'white',
+#   file = '~/Workspace/NTSG/projects/Y2026_PSInet/outputs/rehydration_time_disequilibrium/20250707_RC_circuit_model_RMSE_barplot_alt.png')
+
+i <- 1
+g1 <- NULL
+# for (doy in df.params$DOY.rel) {
+for (doy in 100:111) {
+  g0 <- df.clean %>%
+    filter(DOY.rel == doy) %>%
+    mutate(DOY.rel = sprintf('DOY=%03d', DOY.rel)) %>%
+  ggplot(mapping = aes(x = hour.rel)) +
+    geom_line(aes(y = WP_m), color = 'black', linewidth = 0.6) +
+    # Change X-axis so that the "day" starts at 10h00 local time
+    scale_x_continuous(expand = c(0, 0), breaks = seq(2, 24, 6),
+      labels = function (x) { (x + 14) %% 24 }) +
+    scale_y_continuous(limits = c(-6, -2)) +
+    labs(subtitle = sprintf('DOY=%03d', doy),
+      x = 'Hour of Day (Local Time)', y = 'Water Potential (MPa)')
+
+  g0 <- g0 + with(filter(df.params, DOY.rel == doy),
+      geom_function(fun = model.stomatal, n = max(df.clean$t), xlim = c(0, max(df.clean$t)),
+      # NOTE: Static inputs are indexed at their first value
+      args = list(vpd = Dmean, irr = PAR.MJm2, psi0 = WP_m[1],
+        tau = tau[1], psi.source = psi.source[1], k = k[1], c1 = c1[1], c2 = c2[1]),
+      linetype = 'dashed', color = 'red'))
+  g0 <- g0 + geom_hline(aes(yintercept = psi.source), color = 'blue',
+    linetype = 'dashed', data = filter(df.params, DOY.rel == doy))
+
+  if (i == 1) {
+    g1 <- g0
+  } else {
+    g1 <- g1 + g0
+  }
+  i <- 1 + 1
+}
+
+g1 + plot_layout(nrow = 3, axis_titles = 'collect')
+ggsave(width = 6.5, height = 5.5, dpi = 172,
+  file = '~/Downloads/initial_Buckley_model_fit_by_DOY.png')
